2fb096121082ee577cc202ebc27729bc3b1e1dba
brunnobracioli <brunno_012@hotmail.com>
2022-01-03 21:48:10 -0300

    Insert DTO

--- backend/src/main/java/com/bbtech/dscatalog/resourses/CategoryResource.java
+++ backend/src/main/java/com/bbtech/dscatalog/resourses/CategoryResource.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bbtech.dscatalog.entities.Category;
+import com.bbtech.dscatalog.dto.CategoryDTO;
 import com.bbtech.dscatalog.services.CategoryService;
 
 
@@ -21,8 +21,8 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll(){
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll(){
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
--- backend/src/main/java/com/bbtech/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/bbtech/dscatalog/services/CategoryService.java
@@ -1,11 +1,15 @@
 package com.bbtech.dscatalog.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.bbtech.dscatalog.dto.CategoryDTO;
 import com.bbtech.dscatalog.entities.Category;
 import com.bbtech.dscatalog.repositories.CategoryRepository;
 
@@ -15,9 +19,26 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	/*
+	 A camada de serviço precisa retornar um DTO e para isso recebemos um dado do repository da entidade
+	 Category e convertemos para uma lista de CategoryDTO.
+	 */
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();	
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();	
+		
+		//solução para converter uma lista de Category para uma lista de CategoryDTO
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		
+		/*
+		outra solução para converter uma lista de Category para uma lista de CategoryDTO
+		List<CategoryDTO> listDto = new ArrayList<>();
+		for(Category cat : list) {
+			listDto.add(new CategoryDTO(cat));
+		}
+		
+		return listDto;
+		*/
 	}
 
 }
